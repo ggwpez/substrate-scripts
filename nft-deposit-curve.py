99ff76265f4081d60633ce83ed3d62cccad3c0d3
aurora <aurora.makovac@gmail.com>
2024-02-12 15:17:25 +0100

    adjusted function

--- nft-deposit-curve.py
+++ nft-deposit-curve.py
@@ -3,65 +3,59 @@ import numpy as np
 
 from matplotlib.widgets import Button, Slider
 
-
 # The parametrized function to be plotted
-def f(x, a, b):
-    return 0.2 / (1 + np.exp(a - b * x))
+def f(x, k, x0):
+    # Adjusted to reflect the new function with MIN_DOT and MAX_DOT
+    MIN_DOT = 0.005
+    MAX_DOT = 0.2
+    return MIN_DOT + (MAX_DOT - MIN_DOT) / (1 + np.exp(k * x0 - k * x ))
 
+# Time points to plot
 t = np.linspace(0, 1000000, 100000)
 
-# Define initial parameters
-init_a = 0.1
-init_b = 0.00001
+# a in the code is = x0 and b in the code is = k
+init_k = 0.00001  # Initial steepness
+init_x0 = 500000  # Initial midpoint of the sigmoid
 
-# Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
-line, = ax.plot(t, f(t, init_a, init_b), lw=2)
+line, = ax.plot(t, f(t, init_k, init_x0), lw=2)
 ax.set_xlabel('Collections')
 ax.set_ylabel('Deposit [DOT]')
 
-# adjust the main plot to make room for the sliders
 fig.subplots_adjust(left=0.25, bottom=0.25)
 
-# Make a horizontal slider to control the b.
-axb = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-b_slider = Slider(
-    ax=axb,
-    label='b',
+axk = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+k_slider = Slider(
+    ax=axk,
+    label='Steepness (k)',
     valmin=0.0000001,
-    valmax=0.00002,
-    valinit=init_b,
+    valmax=0.0001,
+    valinit=init_k,
 )
 
-# Make a vertically oriented slider to control the a
-axa = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
-a_slider = Slider(
-    ax=axa,
-    label="a",
-    valmin=0.0,
-    valmax=1,
-    valinit=init_a,
+axx0 = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
+x0_slider = Slider(
+    ax=axx0,
+    label="Midpoint (x0)",
+    valmin=0,
+    valmax=1000000,
+    valinit=init_x0,
     orientation="vertical"
 )
 
-
-# The function to be called anytime a slider's value changes
 def update(val):
-    line.set_ydata(f(t, a_slider.val, b_slider.val))
+    line.set_ydata(f(t, k_slider.val, x0_slider.val))
     fig.canvas.draw_idle()
 
+k_slider.on_changed(update)
+x0_slider.on_changed(update)
 
-# register the update function with each slider
-b_slider.on_changed(update)
-a_slider.on_changed(update)
-
-# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
 resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Reset', hovercolor='0.975')
 
 def reset(event):
-    b_slider.reset()
-    a_slider.reset()
+    k_slider.reset()
+    x0_slider.reset()
 button.on_clicked(reset)
 
 plt.show()
